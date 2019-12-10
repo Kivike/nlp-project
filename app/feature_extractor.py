114f6c13351a03cddee5d968e1a920d0b8e7cd1a
Roope Rajala <coloris23@gmail.com>
2019-12-10 15:40:12 +0000

    Feature extractor for 'unsafe' words

---
+++ app/feature_extractor.py
@@ -0,0 +1,29 @@
+
+class FeatureExtractor:
+
+    def extractFeatures(self, review: dict):
+        value = 0
+
+        # TODO Get synonyms with wordnet?
+        unsafe_synonyms = [
+            "unsafe",
+            "not safe",
+            "risky",
+            "untrustworthy",
+            "alarming",
+            "unreliable",
+            "treacherous",
+            "risky",
+            "perilous"
+        ]
+
+        stars = review['Review Stars']
+
+        if stars <= 2:
+            content = review['Review Content']
+
+            for word in unsafe_synonyms:
+                if word in content:
+                    value += 1
+
+        return value
---
+++ feature_extractor_test.py
@@ -0,0 +1,20 @@
+from app.file.xlsx_reader import XlsxReader
+from app.feature_extractor import FeatureExtractor
+from pandas import read_excel
+import os
+
+extractor = FeatureExtractor()
+script_dir = os.path.dirname(__file__)
+data = read_excel(script_dir + '/datasets/Tripadvisor Review Part1.xlsx')
+
+feature_count = 0
+for index, review in data.iterrows():
+    feature_value = extractor.extractFeatures(review)
+
+    data.loc[data.index[index], 'feature_value'] = feature_value
+
+    if data.at[index, 'feature_value'] > 0:
+        feature_count += 1
+        print(str(index) + " " + str(feature_count))
+
+print(index)
