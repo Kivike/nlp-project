ff2856cbb4d34d793f0c9df942f6fc56690c0ac9
Manish Maharjan <manish@Manishs-MacBook-Pro.local>
2019-12-25 20:10:59 +0545

    Reading hotel address and adding url encoding

--- app/address_to_coordinate.py
+++ app/address_to_coordinate.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import urllib.parse
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
-path = '/datasets/Tripadvisor Review Part1.xlsx'
+
+df = pd.read_excel('../datasets/Tripadvisor Review Part1.xlsx')
 
 # read in dataframe with parse_cols
 # use a list to read in specific columns, even if only one column
 # use 1 instead of 2 since Python is zero indexed
 
 
-class AddressExtractor:
-    def read_xls(self, file_path):
-        dataframe = pd.read_excel(path, review: dict, parse_cols = [2]):
-        second_column = dataframe.iloc[: , 2]
-
-        datacolumn = review['Hotel Address']
+neededColumn = df ['Hotel Address']
+neededColumn2 = urllib.parse.urlencode(neededColumn)
+print(neededColumn2)
+#print(df['Hotel Address'])
