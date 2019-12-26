931d843f205a9943d1ca3a11765fe977254a6a86
Manish Maharjan <manish@Manishs-MacBook-Pro.local>
2019-12-26 22:56:42 +0545

    Half of the URL encoding only works

--- app/address_to_coordinate.py
+++ app/address_to_coordinate.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import urllib
 import urllib.parse
+import numpy as np
+import pyexcel as pe
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
@@ -11,11 +14,51 @@ df = pd.read_excel('../datasets/Tripadvisor Review Part1.xlsx')
 # use 1 instead of 2 since Python is zero indexed
 
 
-neededColumn = df ['Hotel Address']
-neededColumn2 = urllib.parse.urlencode(neededColumn)
+#df = df.loc[df ['Hotel Address'].isnull(), :]
+df = df['Hotel Address']
+#filtered_df = df[df['Hotel Address'].notnull()]
+#df.dropna(subset=['Hotel Address'], inplace=True)
+#print(df)
+#sheet = pe.Sheet([df['Hotel Address']])
+# filter empty
+#neededColumn['Hotel Address'].replace('', np.nan, inplace=True)
+#neededColumn2 = urllib.parse.urlencode(neededColumn)
 #print(neededColumn2)
 #print(df['Hotel Address'])
 
+# filter empty
+#neededColumn['Hotel Address'].replace('', np.nan, inplace=True)
+#neededColumn.dropna(subset=['Hotel Address'], inplace=True)
+
+
+#neededColumn = neededColumn.drop(neededColumn.index[neededColumn.eq('')]) #drop all null rows
+
+#neededColumn = neededColumn.drop(neededColumn.columns[neededColumn.eq('')]) #drop all null columns
+
+#def filter_row(row_index, row):
+#        result = [element for element in row if element != '']
+#        return len(result)==0
+
+#del sheet.row[filter_row]
+#sheet
+#print(sheet)
+
 # First get the each needed rows and print it
-for neededColumnrow in neededColumn:
-    print(neededColumnrow)
+
+for neededColumnrow in df:
+        if neededColumnrow != '':
+#                print("empty list")
+#        else:
+#                print("list is not empty")
+
+    #if neededColumnrow != '':
+
+#value = 0
+#    if ' ' in neededColumnrow:
+#        value -= neededColumnrow
+#    return value
+# Encode the URL
+
+                encodedURL = urllib.parse.quote_plus(neededColumnrow)
+#    encodedURL = urllib.parse.quote_from_bytes(neededColumnrow)
+                print(encodedURL)
