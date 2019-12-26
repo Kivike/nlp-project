b1b19249a1f4bb0a381c8071333b7e9c0d7c833c
ppeerttu <perttu.karna@gmail.com>
2019-12-26 13:27:15 +0200

    gdal via docker

---
+++ geoutils/ogr2ogr.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Wrapper for gdal package running in Docker container
+# 
+# Example usage:
+# cd <directory-on-host-with-shp-files>
+# sh ../<path-to-this-script>/ogr2ogr.sh -f GeoJSON -t_srs crs:84 [name].geojson [name].shp
+# 
+# Any arguments being passed to the script will be passed to the ogr2ogr tool on the container.
+# Use --help command for help (or --long-usage for full help)
+
+HOST_DIR=$(pwd)         # Current directory on the host
+CONTAINER_DIR=/var/tmp  # Working dir on the container
+
+# Bind volume from host to container, set working directory and remove container afterwards
+docker run \
+    -v $HOST_DIR:$CONTAINER_DIR \
+    -w="$CONTAINER_DIR" \
+    --rm \
+    osgeo/gdal:alpine-small-latest ogr2ogr "$@"
