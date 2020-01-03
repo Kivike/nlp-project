ab9d90c90a3825e88c268b990d05af9ec495be49
ppeerttu <perttu.karna@gmail.com>
2020-01-03 15:54:45 +0200

    Finalized the combination of data per sector

--- harmonize_data.py
+++ harmonize_data.py
@@ -6,14 +6,26 @@ import multiprocessing as mp
 from typing import List, Any, Tuple
 from shapely.geometry import shape, Point
 import pandas as pd
+import numpy as np
 from app.map.constants import GJSON_LONDON_SECTORS
 from app.file.utils import read_csv_directory
 from app.time.period import Period
 
-CRIME_DATA_DIR = 'datasets/metropolitan-street'
-HOTEL_DATA_DIR = 'datasets/Hotel_reviews_NLP'
+CRIME_DATA_DIR = os.path.join(os.path.dirname(__file__), 'datasets/metropolitan-street')
+# HOTEL_DATA_DIR = os.path.join(os.path.dirname(__file__), 'datasets/Hotel_reviews_NLP')
+HOTEL_DATA_INPUT = os.path.join(os.path.dirname(__file__), 'datasets/tripadvisor_output.xlsx')
+OUT_DIR = os.path.join(os.path.dirname(__file__), 'datasets/out/')
+FINAL_DATA_OUTPUT = 'final_data.csv'
+SECTORIZED_HOTEL_DATA = 'hotel_reviews_sectorized.csv'
+SECTORIZED_CRIME_DATA = 'crimes_sectorized.csv'
 CPU_COUNT = mp.cpu_count()
-FINAL_DATA_OUTPUT = 'output.csv'
+FEATURE_COLUMNS = [
+    # 'feature_word_unsafe',
+    'sentiment_unsafe',
+    'sentiment_crime',
+    'sentiment_positive',
+    'sentiment_sum'
+]
 
 with open(GJSON_LONDON_SECTORS) as json_file:
     geojson = json.load(json_file)
@@ -70,7 +82,7 @@ def sectorize_zip(x) -> str:
     # Split the parts of post code
     parts = x['ZIP'].split()
     # Concatenate as district + sector number
-    return parts[0] + parts[1][0]
+    return parts[0] + (parts[1][0] if len(parts) > 1 else '')
 
 def sectorize_coordinates(coordinates: List[str]) -> Tuple[dict, List[str]]:
     """Sectorize given coordinates based on globally declared sectors.
@@ -117,17 +129,22 @@ def main():
     print('Loading hotel data...')
     period = Period()
 
-    hotel_data = read_csv_directory(
-        HOTEL_DATA_DIR,
-        filelimit = 1,
-        filetype = 'xlsx'
-    )
-
+    hotel_data = pd.read_excel(HOTEL_DATA_INPUT) # type: pd.DataFrame
     # Drop rows with empty ZIP codes
-    hotel_data = hotel_data.dropna(subset=['ZIP'])
+    hotel_data['ZIP'].replace('', np.nan, inplace = True)
+    hotel_data.dropna(subset=['ZIP'], inplace = True)
+    columns = [
+        'Hotel Name',
+        'Hotel Address',
+        'City',
+        'ZIP',
+        'Review Title',
+        'Review Content',
+        'Review Date'
+    ] + FEATURE_COLUMNS
+    hotel_data = hotel_data[columns]
     period.end()
     print('Hotel data loaded in {}, {} rows\n'.format(period, len(hotel_data)))
-
     #########################
     # Step 2: Read crime data
     #########################
@@ -135,7 +152,7 @@ def main():
     period = Period()
     crime_data = read_csv_directory(
         CRIME_DATA_DIR,
-        filelimit=2
+        columns = ['Latitude', 'Longitude']
     )
 
     # Drop rows with empty locations
@@ -153,6 +170,10 @@ def main():
 
     period.end()
     print('Sectorized hotel data in {}\n'.format(period))
+
+    filename = OUT_DIR + SECTORIZED_HOTEL_DATA
+    hotel_data.to_csv(filename)
+    print('Saved the sectorized hotel data into {}'.format(filename))
 
     ##############################
     # Step 4: Sectorize crime data
@@ -196,14 +217,19 @@ def main():
     period.end()
     print('Sectorized crime data in {}\n'.format(period))
 
-    #####################################
-    # Step 5: Calculate areas for sectors
-    #####################################
-    print('Calculating areas for sectors...')
+    filename = OUT_DIR + SECTORIZED_CRIME_DATA
+    crime_data.to_csv(filename)
+    print('Saved sectorized crime data to {}\n'.format(filename))
+
+    ###########################################
+    # Step 5: Calculate areas for sectors (m^2)
+    ###########################################
+    print('Calculating areas (m^2) for sectors...')
     period = Period()
-    areas = [
-        { 'area': area(feature['geometry']), 'id': feature['id'] } for feature in geojson['features']
-    ]
+    areas = {}
+    for feature in geojson['features']:
+        areas[feature['id']] = area(feature['geometry'])
+
     period.end()
     print('Calculated areas in {}'.format(period))
 
@@ -213,15 +239,49 @@ def main():
     print('Combining data into a DataFrame...')
     period = Period()
 
-    df = hotel_data['sector', 'Review Stars'].groupby(by = 'sector').agg(['sum', 'count'])
+    # Group Hotel data based on sector
+    columns = [
+        'sector'
+    ] + FEATURE_COLUMNS
+    hotel_data = hotel_data[columns]
+    hotel_data.sort_values(by = ['sector'])
+    hgb = hotel_data.groupby('sector') # type: pd.DataFrame
 
-    print(df)
+    counts = hgb.size().to_frame(name = 'review_count')
+
+    # The final data
+    df = (counts
+        .join(hgb.agg({ 'sentiment_unsafe': 'sum' }))
+        .join(hgb.agg({ 'sentiment_crime': 'sum' }))
+        .join(hgb.agg({ 'sentiment_positive': 'sum' }))
+        .join(hgb.agg({ 'sentiment_sum': 'sum' }))
+        .reset_index()) # type: pd.DataFrame
+
+    # Add area per sector
+    df['area'] = df.apply(
+        lambda row: areas[row['sector']] if row['sector'] in areas else np.nan,
+        axis = 1
+    )
+
+    # Group crime data based on sector
+    crime_data = crime_data[['sector']]
+    crime_data.sort_values(by = ['sector'])
+    cgb = crime_data.groupby('sector') # type: pd.DataFrame
+
+    counts = cgb.size().to_frame(name = 'crime_count').reset_index() # type: pd.DataFrame
+    
+    # Join the count of crimes per sector to the final data
+    df = df.join(counts.set_index('sector'), on = 'sector')
+
+    # Drop NaN values
+    df.dropna(subset = ['area', 'crime_count', 'review_count'], inplace = True)
 
     period.end()
-    print('Combined data in {}'.format(period))
+    print('Combined data in {}, {} rows\n'.format(period, len(df)))
 
-    
-
+    filename = OUT_DIR + FINAL_DATA_OUTPUT
+    df.to_csv(filename)
+    print('Saved the final output into {}'.format(filename))
 
 if __name__ == '__main__':
     main()
